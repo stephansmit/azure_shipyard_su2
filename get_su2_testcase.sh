86435f02fa36190ab317ec992e2c88480b6e773a
stephan <stephansmit@hotmail.com>
2019-08-12 13:23:41 +0200

    working version

--- get_su2_testcase.sh
+++ get_su2_testcase.sh
@@ -9,7 +9,7 @@ wget -O APU_turbocharger_master/solution_flow_1.dat https://github.com/su2code/T
 
 #develop branch
 mkdir APU_turbocharger_develop
-wget -O APU_turbocharger_develop/Jones.cfg https://raw.githubusercontent.com/su2code/SU2/raw/develop/TestCases/turbomachinery/APU_turbocharger/Jones.cfg
+wget -O APU_turbocharger_develop/Jones.cfg https://raw.githubusercontent.com/su2code/SU2/develop/TestCases/turbomachinery/APU_turbocharger/Jones.cfg
 wget -O APU_turbocharger_develop/stator.cfg https://github.com/su2code/SU2/raw/develop/TestCases/turbomachinery/APU_turbocharger/stator.cfg
 wget -O APU_turbocharger_develop/rotor.cfg https://github.com/su2code/SU2/raw/develop/TestCases/turbomachinery/APU_turbocharger/rotor.cfg
 wget -O APU_turbocharger_develop/mesh_jones_turbine.su2 https://github.com/su2code/TestCases/raw/develop/turbomachinery/APU_turbocharger/mesh_jones_turbine.su2
